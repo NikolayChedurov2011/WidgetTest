1d8083bc392d0682ac693faeba99139052862421
CORP\n.chedurov <n.chedurov@joyway.games>
2022-11-01 18:52:44 +0700

    Init project

---
+++ Source/WidgetTest/WidgetTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WidgetTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIDGETTEST_API AWidgetTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
